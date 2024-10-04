e7c3bfc1e8221ccf58bb5b20fdc780ae68710cef
judigot <judigot@gmail.com>
2024-10-04 03:20:26 +0000

    chore: install and use i18next react-i18next

---
+++ src/localizations/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from '@/localizations/en.json';
+import ja from '@/localizations/ja.json';
+
+const resources = {
+  English: {
+    translation: en,
+  },
+  Japanese: {
+    translation: ja,
+  },
+};
+
+i18n
+  .use(initReactI18next) // Passes i18n instance to react-i18next
+  .init({
+    resources,
+    lng: 'English', // Default language
+    fallbackLng: 'English', // Fallback language if translation is missing
+    interpolation: {
+      escapeValue: false, // React already escapes output by default
+    },
+  });
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import '@/localizations/i18n'; // Import the i18n configuration
+
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
